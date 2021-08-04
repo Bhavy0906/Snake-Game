5fc09de6c12d32ba4dffd095e55c5cee29842b82
Bhavy0906 <54622886+Bhavy0906@users.noreply.github.com>
2021-08-04 16:07:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ function generateFood() {
 
 function main(ctime) {
     window.requestAnimationFrame(main);
- 
+
     if ((ctime - lastPaintTime) / 1000 < 1 / speed) {
         return;
     }
@@ -115,6 +115,54 @@ function isCollide(sarr) {
     if (sarr[0].y <= 0 || sarr[0].y >= 19) return true;
 }
 
+function swipedetect(el, callback) {
+
+    let touchsurface = el;
+    let swipedir;
+    let startX;
+    let startY;
+    let distX;
+    let distY;
+    let threshold = 150; //required min distance traveled to be considered swipe
+    let restraint = 100; // maximum distance allowed at the same time in perpendicular direction
+    let allowedTime = 300; // maximum time allowed to travel that distance
+    let elapsedTime;
+    let startTime;
+    let handleswipe = callback || function (s) { };
+
+    touchsurface.addEventListener('touchstart', function (e) {
+        var touchobj = e.changedTouches[0]
+        swipedir = 'none'
+        dist = 0
+        startX = touchobj.pageX
+        startY = touchobj.pageY
+        startTime = new Date().getTime() // record time when finger first makes contact with surface
+        e.preventDefault()
+    }, false)
+
+    touchsurface.addEventListener('touchmove', function (e) {
+        e.preventDefault() // prevent scrolling when inside DIV
+    }, false)
+
+    touchsurface.addEventListener('touchend', function (e) {
+        var touchobj = e.changedTouches[0]
+        distX = touchobj.pageX - startX // get horizontal dist traveled by finger while in contact with surface
+        distY = touchobj.pageY - startY // get vertical dist traveled by finger while in contact with surface
+        elapsedTime = new Date().getTime() - startTime // get time elapsed
+        if (elapsedTime <= allowedTime) { // first condition for swipe met
+            if (Math.abs(distX) >= threshold && Math.abs(distY) <= restraint) { // 2nd condition for horizontal swipe met
+                swipedir = (distX < 0) ? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+            }
+            else if (Math.abs(distY) >= threshold && Math.abs(distX) <= restraint) { // 2nd condition for vertical swipe met
+                swipedir = (distY < 0) ? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+            }
+        }
+        handleswipe(swipedir)
+        e.preventDefault()
+    }, false)
+}
+
+
 
 // Main Logic Starts Here
 
@@ -131,10 +179,10 @@ if (hiscore === null) {
 
 let playPromise = musicSound.play();
 if (playPromise !== undefined) {
-    playPromise.catch(function(error) {
-        window.addEventListener('keydown', ()=> musicSound.play());
+    playPromise.catch(function (error) {
+        window.addEventListener('keydown', () => musicSound.play());
     });
-  }
+}
 
 music.addEventListener('click', () => {
     moveSound.play();
@@ -198,3 +246,28 @@ window.addEventListener('keydown', e => {
     }
 })
 
+window.addEventListener('load', function () {
+    swipedetect(board, function (swipedir) {
+        // swipedir contains either "none", "left", "right", "top", or "down"
+        if (swipedir == 'left' && (inputDir.y || (inputDir.x == 0 && inputDir.y == 0))) {
+            moveSound.play();
+            inputDir.x = -1;
+            inputDir.y = 0;
+        }
+        else if (swipedir == 'right' && (inputDir.y || (inputDir.x == 0 && inputDir.y == 0))) {
+            moveSound.play();
+            inputDir.x = 1;
+            inputDir.y = 0;
+        }
+        else if (swipedir == 'up' && (inputDir.x || (inputDir.x == 0 && inputDir.y == 0))) {
+            moveSound.play();
+            inputDir.x = 0;
+            inputDir.y = -1;
+        }
+        else if (swipedir == 'down' && (inputDir.x || (inputDir.x == 0 && inputDir.y == 0))) {
+            moveSound.play();
+            inputDir.x = 0;
+            inputDir.y = 1;
+        }
+    })
+})
